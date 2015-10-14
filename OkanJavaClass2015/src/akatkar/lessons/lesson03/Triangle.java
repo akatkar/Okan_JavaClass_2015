88792de4cb32982839bb452fbf3d733bb971355c
User <User@172.5.1.28>
2015-10-14 11:33:51 +0300

    3.derste yapılanlar

---
+++ OkanJavaClass2015/src/akatkar/lessons/lesson03/Triangle.java
@@ -0,0 +1,70 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package akatkar.lessons.lesson03;
+
+/**
+ *
+ * @author alikatkar
+ */
+public class Triangle {
+
+    //     *
+    //    ***
+    //   *****
+    //  *******
+    // *********
+    public static void leftAllignedTriangle(int len) {
+        for (int i = 0; i < len; i++) {
+            for (int j = 0; j < i + 1; j++) {
+                System.out.print("*");
+            }
+            System.out.println("");
+        }
+    }
+
+public static void rightAllignedTriangle(int len) {
+        for (int i = 0; i < len; i++) {
+            for (int j = 0; j < len; j++) {
+                if(j < len - i - 1){
+                    System.out.print(" ");
+                }else{
+                    System.out.print("*");
+                }
+            }
+            System.out.println("");
+        }
+    }
+    //   *
+    //  ***
+    // *****
+    public static void triangle(int len) {
+        for (int i = 0; i < len; i++) {
+            for (int j = 0; j < len - i; j++) {
+                System.out.print(" ");
+            }
+            for (int j = 0; j < 2 *i +1; j++) {
+                System.out.print("*");
+            }
+            
+            System.out.println("");
+        }        
+    }
+
+    public static void main(String[] args) {
+
+         // iç içe iki for döngüsü kullanarak aşağıdaki şekli ekrana basan
+        // kodu yazın
+        rightAllignedTriangle(5);
+        leftAllignedTriangle(10);
+        triangle(10);
+        
+        int i = 5;
+        i = i << 2;
+        System.out.println("i:"+i);
+        i = i >> 1;
+        System.out.println("i:"+i);
+        
+    }
+}
