c46eef2b1f44df5bb891613e2eca24376903c3de
akatkar <akatkar@ALI-KATKAR.netas.lab.nortel.com>
2015-11-05 09:16:47 +0200

    4.11.2015 Günü derste yapılanlar

---
+++ OkanJavaClass2015/src/akatkar/lessons/lesson05/ArrayExample.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package akatkar.lessons.lesson05;
+
+/**
+ *
+ * @author akatkar
+ */
+public class ArrayExample {
+    
+    
+    public static void main(String[] args){
+        
+        int[] arr = {1,2,3,4,5,6};
+        
+        for(int i=0;i<arr.length;i++){
+            System.out.println("i:"+arr[i]);
+        }
+        arr[5] = 9;        
+        
+        for(int i:arr){
+            System.out.println("i:"+i);
+        }
+        String s1 = "file";
+        String fname = s1 + ".txt";
+        System.out.println("fname:"+fname);
+        
+        s1 += ".txt";
+        System.out.println("s1:"+s1);
+    }
+}
